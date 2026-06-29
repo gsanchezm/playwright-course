06295ab129be905298b01ac340a33984d0af8541
gsanchezm <gilsanchezts@outlook.com>
2026-06-29 13:15:04 -0600

    feat: add MenuPage object and export it through pages index

--- playwright-course/pages/index.ts
+++ playwright-course/pages/index.ts
@@ -2,3 +2,4 @@ export { BasePage } from "./BasePage";
 export { LoginPage } from "./LoginPage";
 export { CatalogPage, type Category } from "./CatalogPage";
 export { CheckoutPage } from "./CheckoutPage";
+export { MenuPage } from "./MenuPage";
